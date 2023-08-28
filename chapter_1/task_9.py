c97f1d8ea599dbe72d4df9d7c888e452eb906c96
CoolCoderCarl <123qwekir2wl@gmail.com>
2023-08-28 22:27:37 +0300

    Fill with comments

--- chapter_1/task_9.py
+++ chapter_1/task_9.py
@@ -1,8 +1,18 @@
 import random
 
+# Generate random list
 task_list_9 = [random.randint(1, 100) for k in range(10)]
-
 print(task_list_9)
 
-print(list(sorted(task_list_9))[-2])
 
+def find_second_max_dig(list_to_find: list) -> int:
+    """
+    Find second max digit in the list
+    :param list_to_find:
+    :return:
+    """
+    return list(sorted(list_to_find))[-2]
+
+
+if __name__ == '__main__':
+    find_second_max_dig(task_list_9)
