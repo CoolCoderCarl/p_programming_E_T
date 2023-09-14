9da2c72385bacc73d9972f1feaa4a8d5c68a6045
CoolCoderCarl <123qwekir2wl@gmail.com>
2023-09-14 17:19:32 +0300

    Fixed provoked err

--- chapter_1/task_9.py
+++ chapter_1/task_9.py
@@ -3,6 +3,8 @@ import random
 # Generate random list
 task_list_9 = [random.randint(1, 100) for k in range(10)]
 print(task_list_9)
+
+
 def find_second_max_dig(list_to_find: list) -> int:
     """
     Find second max digit in the list
